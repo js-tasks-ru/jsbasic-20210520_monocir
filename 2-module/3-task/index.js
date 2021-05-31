73bb06f6042ba2ce3b379d7f0ee49e0d4fbb82a0
monocir <monocir@yandex.ru>
2021-05-31 14:40:10 +0300

    Создан калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
 let calculator = {
-  // ваш код
-};
+  
+  sum() {
+    return this.a + this.b
+  },
+  mul() {
+    return this.a * this.b
+  },
+  read(first, second) {
+    this.a = first;
+    this.b = second;
+  },
+
+}
+
+calculator.read(2, 15);
+console.log( calculator.sum() );
+console.log( calculator.mul() );
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
