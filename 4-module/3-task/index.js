a1e2140498976f03db48f7502be2f40028029d8e
monocir <monocir@yandex.ru>
2021-06-13 19:45:06 +0300

    написана функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,35 @@
 function highlight(table) {
-  // ваш код...
+   // const tableTd = document.getElementsByTagName('table');
+
+   for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    let age = (Number(row.cells[1].innerHTML));
+    if (age <= 18) {
+      row.style.textDecoration = 'line-through';
+    };
+  };
+
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    let gender = row.cells[2].innerHTML;
+    if (gender === 'm') {
+      row.classList.add( 'male' );
+    } else if (gender === 'f') {
+      row.classList.add( 'female' );
+    };
+  };
+
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    let status = row.cells[3].dataset.available;
+    if (status == 'true') {
+      row.classList.add( 'available' );
+    } else if (status == 'false') {
+      row.classList.add( 'unavailable' );
+    console.log(status);
+    } else if (status == undefined) {
+      row.setAttribute('hidden', 'hidden');
+    };
+  };
+
 }
